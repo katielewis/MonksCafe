da0a3bc0650ac0bc9345756cf5aa074ec50ce836
katie <katielewis@katies-iMac.local>
2017-07-26 20:07:53 -0700

    Basic html/css
    All content is in with some minor styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-$('h1').html('Hello from js!');
+function initMap() {
+  var monksLocation = {lat: 40.8054491, lng: -73.9654415};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: monksLocation
+  });
+  var marker = new google.maps.Marker({
+    position: monksLocation,
+    map: map
+    });
+}
