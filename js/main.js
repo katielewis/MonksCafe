358cc8013bc39ad7b906934b58c909a0269ab1c1
katie <katielewis@katies-iMac.local>
2017-07-26 21:16:19 -0700

    More robust css

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ function initMap() {
   var monksLocation = {lat: 40.8054491, lng: -73.9654415};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: monksLocation
+    center: monksLocation,
+    scrollwheel: false
   });
   var marker = new google.maps.Marker({
     position: monksLocation,
